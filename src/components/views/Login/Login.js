83538d8ae789edc3e546d8009473b43de7025c1d
Anka Kozłowska <kozlowskanka@gmail.com>
2020-03-08 10:46:08 +0100

    Login view set

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {Link} from 'react-router-dom';
 
 const Login = () => (
   <div className={styles.component}>
-    <h2>Login view</h2>
+    <TextField className={styles.item} id="standard-basic" label="name" variant="filled" />
+    <TextField className={styles.item}id="standard-basic" label="password" variant="filled" />
+    <Button className={styles.item} component={Link} to={process.env.PUBLIC_URL + '/'} variant="contained" color="primary">Login</Button>
   </div>
 );
 
